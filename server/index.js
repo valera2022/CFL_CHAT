b9306588a47b8b47008c8fd2340fa3111876bab2
Geralmy Urena <geralmyuri11@gmail.com>
2025-04-10 16:47:36 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const http = require("http")
+// const io = require("socket.io")
+const cors = require("cors")
+const {Server} = require("socket.io")
+
+let app = express()
+app.use(cors({
+        origin: "http://localhost:3000",
+        methods: ["GET, POST"],
+        credentials: true
+}))
+let server = http.createServer(app)
+
+let io = new Server(server,{
+     cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET, POST"],
+        credentials: true
+    }
+})
+
+console.log(io)
+
+
+
+    io.on("connection",(socket)=>{
+        console.log("somebody is online", socket.id)
+
+   
+    
+        socket.on("disconnect", ()=>{
+            console.log( "somebody got disconnected", socket.id)
+        })
+       
+    })
+
+
+
+server.listen("3001",()=>{
+    console.log("listening on port 3001")
+})
+
+
